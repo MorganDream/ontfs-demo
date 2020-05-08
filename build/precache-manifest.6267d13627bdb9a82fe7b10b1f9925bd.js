2c13855b136bb2f2452da80c33169b5212068e51
MorganDream <dreameng023@gmail.com>
2020-05-08 13:52:47 +0800

    build

---
+++ build/precache-manifest.6267d13627bdb9a82fe7b10b1f9925bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64a214aca32d96ba2b8c3e57955dc5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20cd1aa6a04c501aac9b",
+    "url": "/static/css/main.dc0a14bb.chunk.css"
+  },
+  {
+    "revision": "c8363d816a8429ec0527",
+    "url": "/static/js/2.5ab305ac.chunk.js"
+  },
+  {
+    "revision": "3790494d1e0dd3fa146996fbd9b9306b",
+    "url": "/static/js/2.5ab305ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20cd1aa6a04c501aac9b",
+    "url": "/static/js/main.34845429.chunk.js"
+  },
+  {
+    "revision": "2d9591520051d7707738",
+    "url": "/static/js/runtime-main.0cbd85f3.js"
+  }
+]);
